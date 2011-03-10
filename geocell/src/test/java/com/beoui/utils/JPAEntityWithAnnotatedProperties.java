4291eafd0e212398c03e886a5a5b107df9774f11
alexandre.gellibert <alexandre.gellibert@b5df4822-22ed-11df-8001-4f4b503b426e>
2011-03-10 21:46:46 +0000

    Merge from JPA-Support Branch to the trunk. Revision 69-70-71
    git-svn-id: http://javageomodel.googlecode.com/svn/trunk@73 b5df4822-22ed-11df-8001-4f4b503b426e

---
+++ geocell/src/test/java/com/beoui/utils/JPAEntityWithAnnotatedProperties.java
@@ -0,0 +1,63 @@
+package com.beoui.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.beoui.geocell.annotations.Geocells;
+import com.beoui.geocell.annotations.Latitude;
+import com.beoui.geocell.annotations.Longitude;
+
+@Entity
+public class JPAEntityWithAnnotatedProperties {
+
+	String id;
+
+	@Longitude
+	double longitude;
+
+	@Latitude
+	double latitude;
+
+	@Geocells
+	@OneToMany(fetch = FetchType.EAGER)
+    private List<String> geoCellsData = new ArrayList<String>();
+
+	@Id
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public List<String> getGeoCellsData() {
+    	return geoCellsData;
+    }
+
+	public void setGeoCellsData(List<String> geocells) {
+    	this.geoCellsData = geocells;
+    }
+
+}
